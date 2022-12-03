02d520ec864ac5df34958db8addbad5277a43a63
Jiří Vrba <jiri@vrba.dev>
2022-12-03 19:22:48 +0100

    Implement day2 / taks 2

--- day02/index.ts
+++ day02/index.ts
@@ -6,6 +6,12 @@ enum Shape {
   Scissors = "scissors",
 }
 
+enum Outcome {
+  Draw = "draw",
+  Win = "win",
+  Lost = "lost",
+}
+
 type OpponentSymbol = "A" | "B" | "C";
 type PlayerSymbol = "X" | "Y" | "Z";
 
@@ -21,6 +27,12 @@ const playerMappings: Record<PlayerSymbol, Shape> = {
   Z: Shape.Scissors,
 };
 
+const outcomeMappings: Record<PlayerSymbol, Outcome> = {
+  X: Outcome.Lost,
+  Y: Outcome.Draw,
+  Z: Outcome.Win,
+};
+
 interface ParsedLine {
   opponent: Shape;
   player: Shape;
@@ -33,37 +45,75 @@ const baseScores: Record<Shape, number> = {
 };
 
 const beats: Record<Shape, Shape> = {
-    [Shape.Rock]: Shape.Scissors,
-    [Shape.Paper]: Shape.Rock,
-    [Shape.Scissors]: Shape.Paper,
+  [Shape.Rock]: Shape.Scissors,
+  [Shape.Paper]: Shape.Rock,
+  [Shape.Scissors]: Shape.Paper,
 };
 
 const source = Deno.readTextFileSync("./input.txt")
   .split("\n")
   .map((line) => line.trim())
   .filter((line) => line.length > 0)
-  .map((line) => line.split(/\s+/))
+  .map((line) => line.split(/\s+/));
+
+const evaluate = (opponent: Shape, player: Shape): number => {
+  const base = baseScores[player];
+  // draw
+  if (opponent === player) {
+    return base + 3;
+  }
+
+  // win
+  if (beats[player] === opponent) {
+    return base + 6;
+  }
+
+  return base;
+};
+
+// Part 1
+const part1Score = source
   .map(([opponent, player]): ParsedLine => {
     return {
       opponent: opponentMappings[opponent as OpponentSymbol],
       player: playerMappings[player as PlayerSymbol],
     };
-  });
+  })
+  .reduce(
+    (total, { opponent, player }) => total + evaluate(opponent, player),
+    0
+  );
 
-const score = source.reduce((total, {opponent, player}) => {
-    const base = baseScores[player];
-    // draw
-    if (opponent === player) {
-        return total + base + 3;
-    }
+console.log(part1Score);
 
-    // win 
-    if (beats[player] === opponent) {
-        return total + base + 6;
-    }
+// Part 2
+const invertedBeats: Record<Shape, Shape> = Object.entries(beats).reduce((acc, [key, value]) => { return {...acc, [value]: key}}, {}) as Record<Shape, Shape>;
+const findMatchingShape = (opponent: Shape, outcome: Outcome): Shape => {
+  if (outcome === Outcome.Draw) {
+    return opponent;
+  }
 
-    return total + base;
-}, 0);
+  if (outcome === Outcome.Win) {
+    return invertedBeats[opponent];
+  }
 
-// Part 1
-console.log(score);
+  return beats[opponent];
+};
+
+const part2Score = source
+  .map(([opponent, player]): ParsedLine => {
+    const outcome = outcomeMappings[player as PlayerSymbol];
+    const opponentShape = opponentMappings[opponent as OpponentSymbol];
+    const playerShape = findMatchingShape(opponentShape, outcome);
+
+    return {
+      opponent: opponentShape,
+      player: playerShape,
+    };
+  })
+  .reduce(
+    (total, { opponent, player }) => total + evaluate(opponent, player),
+    0
+  );
+
+console.log(part2Score);
